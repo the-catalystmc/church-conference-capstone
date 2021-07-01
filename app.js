fbda204c4b02062c624f002cfa81cc137de80d0c
Rhyine <rhyinestewart@gmail.com>
2021-06-30 20:07:35 -0500

    Add dynamic javascript loading for speakers section.

--- app.js
+++ app.js
@@ -30,3 +30,51 @@ menuItem.addEventListener('click', (e) => {
       }
 })
 
+// -------CODE TO LOAD PAGE DYNAMICALLY---------
+const speakerList = document.querySelector('.speaker__cards');
+const speakerTemplate = document.querySelector('.speaker__template');
+
+const myData = [{
+    speakerImg: 'images/profilepic/pexels-andrea-piacquadio-3789888.jpg',
+    speakerName: 'Martin Roon',
+    speakerTitle: 'Author',
+    speakerDesc: 'Martin Roon shares captivating stories of a powerful God with churches, conferences, prisons, and other venues around the country, year-round.',
+  },
+  {
+    speakerImg: 'images/profilepic/pexels-thyrone-paas-1722198.jpg',
+    speakerName: 'Charles Depass',
+    speakerTitle: 'Bishop',
+    speakerDesc: 'A missionary sent to spread the Lords holy word and his teachings.',
+  },
+  {
+    speakerImg: 'images/profilepic/pexels-andrea-piacquadio-762020.jpg',
+    speakerName: 'Maya Havanu',
+    speakerTitle: 'Lecturer at Harvard',
+    speakerDesc: 'A missionary sent to spread the Lords holy word and his teachings.',
+  },
+  {
+    speakerImg: 'images/profilepic/pexels-yulia-rozanova-3555630.jpg',
+    speakerName: 'Yamish Irea',
+    speakerTitle: 'Motivational Speaker',
+    speakerDesc: 'A missionary sent to spread the Lords holy word and his teachings.',
+  }
+]
+
+const createSpeakerCard = (speakerData) => {
+    const clone = speakerTemplate.content.firstElementChild.cloneNode(true);
+    clone.querySelector('.speaker__pic').src = speakerData.speakerImg;
+    clone.querySelector('.speaker__name').innerText = speakerData.speakerName;
+    clone.querySelector('.speaker__qual').innerText = speakerData.speakerTitle;
+    clone.querySelector('.speaker__desc').innerText = speakerData.speakerDesc;
+
+
+    speakerList.appendChild(clone);
+}
+
+const setup = () => {
+    myData.forEach((data) => {
+        createSpeakerCard(data);
+    })
+}
+
+setup();
